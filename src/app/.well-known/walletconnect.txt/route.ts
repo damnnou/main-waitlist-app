afbf4a0b672536c2d402cdac924e16272af97188
Kirill S <skdamn@outlook.com>
2025-06-10 11:27:19 +0300

    verify

--- src/app/.well-known/walletconnect.txt/route.ts
+++ src/app/.well-known/walletconnect.txt/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     try {
-        const textContent = "";
+        const textContent = "d6b8ff20-9062-42ef-b21a-12876086e918=733517304bddd5fdf958bdcbe5dab3e99112ffb99df580a33a058581fa0b020c";
 
         return new NextResponse(textContent, {
             status: 200,
